90d79f105796014b0a75672b944250f1abbef992
Faustina Maria <faustina.giaquinta@gmail.com>
2020-09-06 20:41:47 -0400

    added icons, moved navbar and breadcrumbs to separated templates

--- app/icons.py
+++ app/icons.py
@@ -1,28 +1,14 @@
 import pickle
 
 icons = {
-    'folder': """<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-folder" fill="currentColor" xmlns="http: //www.w3.org/2000/svg">
-  <path d="M9.828 4a3 3 0 0 1-2.12-.879l-.83-.828A1 1 0 0 0 6.173 2H2.5a1 1 0 0 0-1 .981L1.546 4h-1L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3v1z"/>
-  <path fill-rule="evenodd" d="M13.81 4H2.19a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4zM2.19 3A2 2 0 0 0 .198 5.181l.637 7A2 2 0 0 0 2.826 14h10.348a2 2 0 0 0 1.991-1.819l.637-7A2 2 0 0 0 13.81 3H2.19z"/>
-</svg>""",
-    'file': """<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-file-earmark-text" fill="currentColor" xmlns="http: //www.w3.org/2000/svg">
-  <path d="M4 0h5.5v1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h1V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"/>
-  <path d="M9.5 3V0L14 4.5h-3A1.5 1.5 0 0 1 9.5 3z"/>
-  <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
-</svg>""",
-    '.jpg': """<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-file-image" fill="currentColor" xmlns="http: //www.w3.org/2000/svg">
-  <path fill-rule="evenodd" d="M4 0h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v10l2.224-2.224a.5.5 0 0 1 .61-.075L8 11l2.157-3.02a.5.5 0 0 1 .76-.063L13 10V2a1 1 0 0 0-1-1H4z"/>
-  <path fill-rule="evenodd" d="M6.502 7a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
-</svg>""",
-    '.pdf': """<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-file-earmark-richtext" fill="currentColor" xmlns="http: //www.w3.org/2000/svg">
-  <path d="M4 0h5.5v1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h1V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"/>
-  <path d="M9.5 3V0L14 4.5h-3A1.5 1.5 0 0 1 9.5 3z"/>
-  <path fill-rule="evenodd" d="M4.5 12.5A.5.5 0 0 1 5 12h3a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zm0-2A.5.5 0 0 1 5 10h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zm1.639-3.708l1.33.886 1.854-1.855a.25.25 0 0 1 .289-.047l1.888.974V8.5a.5.5 0 0 1-.5.5H5a.5.5 0 0 1-.5-.5V8s1.54-1.274 1.639-1.208zM6.25 6a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5z"/>
-</svg>""",
-    'no-icon': """<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-file-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-  <path fill-rule="evenodd" d="M4 0h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H4z"/>
-  <path fill-rule="evenodd" d="M6.146 6.146a.5.5 0 0 1 .708 0L8 7.293l1.146-1.147a.5.5 0 1 1 .708.708L8.707 8l1.147 1.146a.5.5 0 0 1-.708.708L8 8.707 6.854 9.854a.5.5 0 0 1-.708-.708L7.293 8 6.146 6.854a.5.5 0 0 1 0-.708z"/>
-</svg>"""}
+    'folder': 'folder',
+    '.jpg': "file-image",
+    '.pdf': "file-earmark-richtext",
+    '.ppt': '',
+    '.xls': '',
+    '.text': "file-earmark-text",
+    'no-icon': "file-x"
+}
 
 
 def update_icons():
--- app/main.py
+++ app/main.py
@@ -4,10 +4,20 @@ from icons import update_icons
 import os
 app = Flask(__name__)
 
-if 'icon.pkl' not in os.listdir():
-    update_icons()
+# update_icons()
 
-icon = pickle.load(open('icon.pkl', 'rb'))
+#icon = pickle.load(open('icon.pkl', 'rb'))
+
+icon = {
+    'folder': 'folder',
+    '.jpg': "file-image",
+    '.pdf': "file-earmark-richtext",
+    '.ppt': '',
+    '.xls': '',
+    '.text': "file-earmark-text",
+    'no-icon': "file-x"
+}
+
 
 test = [{'file': 'Folder A', 'topic_x': 'hello',
          'absolute_path': '#', 'extension': 'folder'},
@@ -18,12 +28,15 @@ test = [{'file': 'Folder A', 'topic_x': 'hello',
         {'file': 'filename2', 'topic_x': 'hello',
          'absolute_path': '#', 'extension': '.xls'},
         {'file': 'image something', 'topic_x': 'hello',
-         'absolute_path': '#', 'extension': '.jpg'}]
+         'absolute_path': '#', 'extension': '.jpg'},
+        {'file': 'image something', 'topic_x': 'hello',
+         'absolute_path': '#', 'extension': '.text'}]
 
 
 @ app.route('/<name>')
 def index(name, rows=test):
     name = name.capitalize()
+    os.chdir('')
 
     return render_template('index.html', name=name, rows_len=len(test), rows=test, icon=icon)
 
