a22fd33683d18a0251896f7e39cc2a5cdbbcb6b4
Faustina Maria <faustina.giaquinta@gmail.com>
2020-09-09 08:35:27 -0400

    added check for slash in folder, deleted count of files (done from html), refactored blind_categories to use with session information instead of analyzing when called

--- app/helpers.py
+++ app/helpers.py
@@ -4,9 +4,13 @@ from datetime import datetime
 
 
 def get_attributes(filepath, folder):
+    if not folder.endswith('/'):
+        folder = folder + '/'
+
     file = folder + filepath
     file_stats = os.stat(file)
     attributes = {'parent_folder': folder,
+                  'absolute_path': file,
                   'name': filepath,
                   'size_mb': file_stats.st_size / 1e+6,
                   'created_on': datetime.fromtimestamp(file_stats.st_birthtime),
@@ -14,7 +18,6 @@ def get_attributes(filepath, folder):
 
     if os.path.isdir(file):
         ext = 'folder'
-        attributes['nfiles'] = len(os.listdir(file))
     elif os.path.isfile(file):
         ext = '.' + filepath.split('.')[-1]
     else:
@@ -29,7 +32,6 @@ def blind_categories(format_dict, files):
     blind_classifier = {}
 
     for k, v in format_dict.items():
-        blind_classifier[k] = [
-            e for e in files if (f".{e.split('.')[-1]}" in v)]
+        blind_classifier[k] = [e for e in files if (e['extension'] in v)]
 
     return blind_classifier
