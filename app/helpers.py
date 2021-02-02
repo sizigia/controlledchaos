a9e81b52d53e663a39a795fd63e7797d7126ab76
Faustina Maria <faustina.giaquinta@gmail.com>
2021-02-02 15:15:00 +0000

    adding project for review

--- app/helpers.py
+++ app/helpers.py
@@ -1,6 +1,15 @@
+from tika import parser
 import os
 import re
 from datetime import datetime
+from bs4 import BeautifulSoup
+import chardet
+import textract
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+from collections import Counter, defaultdict
+from flask import redirect, url_for
 
 
 def get_attributes(filepath, folder):
@@ -12,14 +21,14 @@ def get_attributes(filepath, folder):
     attributes = {'parent_folder': folder,
                   'absolute_path': file,
                   'name': filepath,
-                  'size_mb': file_stats.st_size,  # / 1e+6 #MB,
+                  'size_mb': file_stats.st_size / 1e+6,
                   'created_on': datetime.fromtimestamp(file_stats.st_birthtime),
                   'last_modified_on': datetime.fromtimestamp(file_stats.st_mtime)}
 
     if os.path.isdir(file):
         ext = 'folder'
     elif os.path.isfile(file):
-        ext = '.' + filepath.split('.')[-1]
+        ext = filepath.split('.')[-1]
     else:
         ext = ''
 
@@ -28,13 +37,75 @@ def get_attributes(filepath, folder):
     return attributes
 
 
-def blind_categories(format_dict, files):
-    blind_classifier = {}
+def blind_categories(format_dict, collection):
+    blind_classifier = {k: list(collection.find(
+        {"extension": {"$in": v}})) for k, v in format_dict.items()}
 
-    for k, v in format_dict.items():
-        blind_classifier[k] = [e for e in files if (e['extension'] in v)]
+    for k, v in blind_classifier.items():
+        [(file.pop('_id', None) and file.pop('text', None)) for file in v]
 
-    blind_classifier['folders'] = [
-        e for e in files if (e['extension'] == 'folder')]
+    blind_classifier = {item: blind_classifier[item] for item in sorted(
+        blind_classifier, key=lambda i: len(blind_classifier[i]), reverse=True)}
 
     return blind_classifier
+
+
+def extract_text(record, thresh=1000):
+    text_ext = ['ppt', 'pptx', 'doc', 'docx', 'odt', 'pdf',
+                'rtf', 'text', 'txt', 'wpd', 'ods', 'xls', 'xlsm', 'xlsx']
+
+    if record['extension'] in text_ext:
+        text = parser.from_file(record['absolute_path'], service='text')[
+            'content'][:thresh]
+
+    return text
+
+
+def labels(corpus):
+    params = {
+        'vectorizer': {
+            'analyzer': 'word',
+            'stop_words': stopwords.words('english'),
+            'ngram_range': (1, 1),
+            'token_pattern': '[a-z]{3,}',
+            'max_df': 0.1,
+            'lowercase': True
+        },
+        'km_params': {
+            'random_state': 30,
+            'n_clusters': 10,
+        }
+    }
+
+    tfidf = TfidfVectorizer(**params['vectorizer'])
+    try:
+        X = tfidf.fit_transform(corpus)
+        km = KMeans(**params['km_params']).fit(X)
+        return km.labels_
+    except:
+        return 404
+
+
+def name_cluster_labels_common(sw, topic_files, words=8):
+    cluster_names = {}
+
+    for i in topic_files.keys():
+        label_text = ' '.join([file['text'] for file in topic_files[i]])
+        corpus = re.findall('[a-zA-Z]{4,}', label_text)
+        corpus = [w for w in corpus if (w.lower() not in sw)]
+        label_dict = Counter(corpus)
+        cluster_names[i] = '-'.join([w[0]
+                                     for w in label_dict.most_common(words)])
+
+    return cluster_names
+
+
+def get_topic_files(files):
+    topic_files = defaultdict(list)
+
+    for file in files:
+        topic_files[file['topic']].append(file)
+
+    topic_files = dict(topic_files)
+
+    return topic_files
--- app/main.py
+++ app/main.py
@@ -1,60 +1,64 @@
 import os
 import pickle
 import re
+from collections import Counter, defaultdict
 from datetime import datetime
 
+import chardet
 import pandas as pd
+import pymongo
+import textract
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 from flask import (Flask, flash, redirect, render_template, request, session,
                    url_for)
+from nltk.corpus import stopwords
+from sklearn.cluster import KMeans
+from sklearn.feature_extraction.text import TfidfVectorizer
+from tika import parser
 
-from helpers import blind_categories, get_attributes
+from helpers import (blind_categories, extract_text, get_attributes,
+                     get_topic_files, labels, name_cluster_labels_common)
+
+project_folder = os.path.expanduser(
+    '~/METIS/BOOTCAMPWORK/Project5/controlledchaos/app/')  # the folder of your project
+load_dotenv(os.path.join(project_folder, '.env'))
+
+MONGODBNAME = os.getenv("MONGODBNAME")
+
+client = pymongo.MongoClient()
+db = client[MONGODBNAME]
+curr_files_db = db['curr_files']
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mockupkey'
+app.config['SESSION_TYPE'] = 'filesystem'
 
 
 icon = {
     'folder': 'folder',
-    '.jpg': "file-image",
-    '.pdf': "file-earmark-richtext",
-    '.html': "file-post",
-    '.ppt': 'file-slides',
-    '.xls': 'file-spreadsheet',
-    '.text': "file-earmark-text",
-    '.doc': 'file-earmark-font',
+    'jpg': "file-image",
+    'pdf': "file-earmark-richtext",
+    'html': "file-post",
+    'ppt': 'file-slides',
+    'xls': 'file-spreadsheet',
+    'text': "file-earmark-text",
+    'txt': "file-earmark-text",
+    'doc': 'file-earmark-font',
     'no-icon': "file-x"
 }
 
-formats = {
-    'audio_vid': ['.aif', '.cda', '.mid', '.midi', '.mp3', '.m4a', '.mpa', '.ogg', '.wav', '.wma', '.wpl',
-                  '.3g2', '.3gp', '.avi', '.flv', '.h264', '.m4v', '.mkv', '.mov', '.mp4', '.mpg', '.mpeg',
-                  '.rm', '.swf', '.vob', '.wmv'],
-    'compressed': ['.7z', '.arj', '.deb', '.pkg', '.rar', '.rpm', '.tar.gz', '.z', '.zip'],
-    'disk_img': ['.bin', '.dmg', '.iso', '.toast', '.vcd'],
-    'data_dbs': ['.csv', '.dat', '.db', '.dbf', '.log', '.mdb', '.sav', '.sql', '.tar', '.xml'],
-    'email': ['.email', '.eml', '.emlx', '.msg', '.oft', '.ost', '.pst', '.vcf'],
-    'executable': ['.apk', '.bat', '.bin', '.cgi', '.pl', '.com', '.exe', '.gadget', '.jar', '.msi', '.py', '.wsf'],
-    'font': ['.fnt', '.fon', '.otf', '.ttf'],
-    'image': ['.ai', '.bmp', '.gif', '.ico', '.jpeg', '.jpg', '.png', '.ps', '.psd', '.svg', '.tif', '.tiff'],
-    'internet': ['.asp', '.aspx', '.cer', '.cfm', '.cgi', '.pl', '.css', '.htm', '.html', '.js', '.jsp',
-                 '.part', '.php', '.py', '.rss', '.xhtml'],
-    'presentation': ['.key', '.odp', '.pps', '.ppt', '.pptx'],
-    'text': ['.doc', '.docx', '.odt', '.pdf', '.rtf', '.tex', '.txt', '.wpd', '.ods', '.xls', '.xlsm', '.xlsx'],
-    'system': ['.bak', '.cab', '.cfg', '.cpl', '.cur', '.dll', '.dmp', '.drv', '.icns', '.ico', '.ini', '.lnk', '.msi', '.sys', '.tmp']
-}
-
-form_names = {'folders': 'Folders', 'audio_vid': 'Media', 'compressed': 'Compressed', 'disk_img': 'Disk images', 'data_dbs': 'Data and Databases', 'email': 'Email related files',
+formats = pickle.load(open('data/formats.pkl', 'rb'))
+form_names = {'folder': 'Folders', 'audio_vid': 'Media', 'compressed': 'Compressed', 'disk_img': 'Disk images', 'data_dbs': 'Data and Databases', 'email': 'Email related files',
               'executable': 'Programs', 'font': 'Fonts', 'image': 'Pictures', 'internet': 'Web related files', 'presentation': 'Slides', 'text': 'Text documents', 'system': 'Settings'}
 
-
-test = [{'file': 'some file', 'topic_x': 'hello',
-         'absolute_path': '../data/t5/000002.doc', 'extension': '.html'}]
+custom_sw = pickle.load(open('data/stopwords.pkl', 'rb'))
 
 
 @ app.route('/', methods=['POST'])
 def open_file():
     filepath = request.form.get('filepath')
-    os.popen(f"open {filepath}")
+    os.popen('open "{}"'.format(filepath))
 
     return '', 204
 
@@ -62,57 +66,69 @@ def open_file():
 @ app.route('/select-folders', methods=['POST'])
 def index_folders():
     if request.method == 'POST':
-        session['curr_folder'] = request.form.getlist('folder')
+        folders = request.form.getlist('folder')
+        if not len(folders):
+            folders = session['curr_folder']
+
+    if (folders != session['curr_folder']):
+        curr_files_db.delete_many({})
+
+        all_files = []
+
+        for folder in folders:
+            files = os.listdir(folder)[:200]
+            curr_files_db.insert_many(
+                [get_attributes(file, folder) for file in files])
+
+        session['curr_folder'] = folders
 
     return '', 204
 
 
 @ app.route('/')
 def index():
-    home_folder = '/Users/faustina/METIS/BOOTCAMPWORK'
+    # hardcoded
+    #home_folder = '/Users/faustina/METIS/BOOTCAMPWORK/Project5/controlledchaos/data/'
+    home_folder = '/Users/faustina/Documents/'
+
     if home_folder[-1] != '/':
         home_folder += '/'
-    if ('curr_folder' not in session) or (not len(session['curr_folder'])):
-        session['curr_folder'] = [home_folder]
+
+    session['curr_folder'] = [home_folder]
 
     breadcrumbs = home_folder[1:].split('/')
-    session['file_attrs'] = [get_attributes(file, home_folder) for file in os.listdir(
+    folders_attrs = [get_attributes(file, home_folder) for file in os.listdir(
         home_folder) if os.path.isdir(home_folder + file)]
 
-    no_files = len(session['file_attrs'])
+    nfolders = len(folders_attrs)
+    session['folder_attrs'] = folders_attrs
 
-    return render_template('index.html', current_folder=home_folder, breadcrumbs=breadcrumbs, nfiles=no_files, files=session['file_attrs'], icon=icon)
+    # flash when there are no folders
+
+    return render_template('index.html', current_folder=home_folder, breadcrumbs=breadcrumbs, nfolds=nfolders, files=folders_attrs, icon=icon)
 
 
 @ app.route('/home')
 def homepage():
-    folders = session['curr_folder']
-    all_files = []
+    cursor = curr_files_db.find({})
+    entries = list(cursor)
 
-    for folder in folders:
-        files = os.listdir(folder)
-        all_files.extend([get_attributes(file, folder) for file in files])
-
-    session['file_attrs'] = all_files
-
-    return render_template('homepage.html', files=session['file_attrs'], icon=icon)
+    return render_template('homepage.html', files=entries, icon=icon, format_names=form_names)
 
 
 @app.route('/composition')
 def composition():
-    files_w_attributes = session['file_attrs']
-
-    blind_cats = blind_categories(formats, files_w_attributes)
-    blind_cats = {item: blind_cats[item] for item in sorted(
-        blind_cats, key=lambda i: len(blind_cats[i]), reverse=True)}
+    # print(request.url_rule.rule)
+    blind_cats = blind_categories(formats, curr_files_db)
 
     return render_template('composition.html', icon=icon, form_names=form_names, blind_categories=blind_cats)
 
 
 @app.route('/indexer')
 def indexer():
+    topic_files = pickle.load(open('data/This_Directory_is_a_Mess.pkl', 'rb'))
 
-    return render_template('indexer.html')
+    return render_template('indexer.html', topic_files=topic_files, icon=icon)
 
 
 if __name__ == '__main__':
